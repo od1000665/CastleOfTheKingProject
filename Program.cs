c034f6d92993f6dbf2fe2864c326acbf9229aab4
od1000665 <od1000665@otc.edu>
2025-09-29 19:36:11 -0500

    Pseudocode
    rough draft of Pseudocode

--- Program.cs
+++ Program.cs
@@ -2,3 +2,26 @@
 Created by: Owen Douglas
 A small mini game prompting you to guess doors in order to find the king, treasure or be eaten by a dragon
 */
+
+//Intro
+
+
+//Will make the random number varible 
+
+
+//Ask player to choose a number 1-3
+
+
+//While Loop to keep the game running
+
+
+//Add doors opened
+
+//Door Outcomes
+
+
+
+//Ask to play again
+
+
+//End
